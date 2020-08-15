52bcdcdedf5b15aeb12ea40819df3ffbeb7cc0c9
Alina Shokota <alina.shokota159@gmail.com>
2020-08-15 15:33:18 +0200

    initial project

---
+++ src/main/java/com/temperature/smart/controller/TemperatureController.java
@@ -0,0 +1,25 @@
+package com.temperature.smart.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/data")
+@CrossOrigin(origins = "*")
+public class TemperatureController {
+
+    @GetMapping("/all")
+    public ResponseEntity<List<String>> getAllData(){
+        List<String> list = new ArrayList<>();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        return ResponseEntity.ok(list);
+    }
+}
